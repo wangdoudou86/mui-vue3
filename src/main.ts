875209ad89b6045f017f5b108440f7b189245a20
wangting <892184375@qq.com>
2022-02-10 19:02:44 +0800

    创建文档首页

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.scss'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+
+const history = createWebHashHistory()
+const router = createRouter({
+    history: history,
+    routes: [
+        {path: '/', component: Home},
+    {path: '/doc', component: Doc}
+    ]
+})
+
+
+
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
