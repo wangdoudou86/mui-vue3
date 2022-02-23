bc46978448f32d099d3ef85f10610d9ea50ee9b7
darkTi <892184375@qq.com>
2022-02-23 17:46:42 +0800

    [Dialog]初始化Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-      <Dialog />
-  </div>
+  
+    <div>Dialog示例</div>
+    <h1>示例一</h1>
+    <button @click="toggle">toggle</button>
+      <Dialog v-model:visible="x" />
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import Dialog from '../lib/Dialog.vue';
+
 export default {
-  components:{Dialog}
+  components:{Dialog},
+  setup(){
+    const x = ref(false)
+    const toggle = ()=>{
+      x.value = !x.value
+    }
+
+    return {
+      x,
+      toggle
+    }
+  }
 }
 </script>
 <style lang='scss' scoped>
