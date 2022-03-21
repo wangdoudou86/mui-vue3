8db79a4783f7459cb525b130c59b4905e5b1a86d
wangting <892184375@qq.com>
2022-03-21 22:13:45 +0800

    Tabs Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-      tabs 示例
+  <div class="example">
+    <h1>示例一</h1>
+    <Tabs>
+      <Tab title='导航一'>内容一</Tab>
+      <Tab title='导航二'>内容二</Tab>
+      <Tab title='导航三'>内容三</Tab>
+    </Tabs>
   </div>
 </template>
 
 <script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
 export default {
-
-}
+  components: { Tabs, Tab },
+};
 </script>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+.example {
+  margin-bottom: 30px;
+  h1 {
+    font-size: 20px;
+    margin: 10px 0;
+  }
+  .decorate {
+    font-size: 16px;
+    color: #666666;
+    margin: 10px 0;
+  }
+}
 </style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      Tabs组件
+      <component :is="defaults[0]" />
+      <component :is="defaults[1]" />
+      <component :is="defaults[2]" />
+  </div>
+</template>
+
+<script>
+export default {
+ setup(props, context){
+     console.log(context.slots.default(), 'xxx')
+     const defaults = context.slots.default()
+     return {defaults}
+ }
+}
+</script>
+<style lang='scss' scoped>
+</style>
